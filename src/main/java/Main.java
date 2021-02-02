3b5082cbedc75e2cf904cc4a9ccf9a8dfe0686ca
Robert Andreev <andreev.robert.a@gmail.com>
2021-02-02 20:24:59 +0500

    Small fix input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Example: ((2.5*-3)-(10.3-4+(-3)))/4");
         System.out.println("Enter 'q' for exit");
         do {
-            System.out.print("Expression: ");
+            System.out.println("Expression: ");
             String input = scanner.next();
             if (input.equals("q"))
                 break;
